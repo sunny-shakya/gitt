84fa2807ad9111e4528a89011cb340d23871852e
sunny shakya <shakya.sunny4@gmail.com>
2012-09-07 05:22:12 -0700

    Update db.php
    2 update

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $mysql_user = "root";
 $mysql_password = "";
 $mysql_database = "imagedb";
 $prefix = "";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps sadasda some thing went wrong");
 mysql_select_db($mysql_database, $bd) or die("Opps some thing wsfdafdasfv ent wrong");
 
 ?>
